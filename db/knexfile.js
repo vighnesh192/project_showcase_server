a94a51ebc7d5fa3c77c0038cebc109c9a7180fef
vighnesh192 <vighnesh192000@gmail.com>
2021-11-03 20:33:24 +0530

    Fix: Knex config for production

--- db/knexfile.js
+++ db/knexfile.js
@@ -28,5 +28,17 @@ module.exports = {
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL 
-  }
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations',
+    directory: './migrations'
+  },
+  seeds: {
+    directory: './seeds'
+  },
+  ...knexSnakeCaseMappers
 };
